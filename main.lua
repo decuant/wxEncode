52b54fc7b4ef316ccb1eb60ca309f5977e53cd55
decuant <decuant@gmail.com>
2018-11-20 11:09:34 +0100

    tidy up
    moved the create test file out
    tidy up

--- main.lua
+++ main.lua
@@ -4,13 +4,13 @@
 --
 -- ----------------------------------------------------------------------------
 
-local trace		= require "trace"			-- shortcut for tracing
+local trace		= require "trace"		-- shortcut for tracing
 local window	= require "window"		-- GUI for the application
-local rnd		= require "random"		-- random number generator
+local tUtf8Code = require "Utf8Table"	-- utf_8 codes table
+local samples	= require "samples"		-- create a list of Unicode chars
 
 local _toChar	= string.char
 local _format	= string.format
-local _rand		= rnd.getInRange
 
 -- ----------------------------------------------------------------------------
 --
@@ -19,20 +19,19 @@ local function CountLines()
 	
 	local iLimit	= thisApp.iMemorySize
 	local sSource	= thisApp.sFileMemory
-	local iRows		= 0 
-	local iEnd	 	= sSource:find("\n", 1, true)
+	
+	if 0 >= iLimit then return 0 end
+	
+	local iRows	= 1
+	local iEnd	= sSource:find("\n", 1, true)
 	
 	-- total rows
 	--
 	while iEnd do
 		
 		iRows = iRows + 1
-		iEnd	= sSource:find("\n", iEnd + 1, true)
+		iEnd  = sSource:find("\n", iEnd + 1, true)
 	end
-	
-	-- update
-	--
-	thisApp.iNumOfRows = iRows
 	
 	return iRows
 end
@@ -71,7 +70,7 @@ local function OnLoadFile()
 	
 	-- do a scan of text
 	--
-	CountLines()
+	thisApp.iNumOfRows = CountLines()
 	
 	local sText = _format("Read [file: %s] [mode: %s] [lines: %d] [bytes: %d]", 
 								 sSourceFile, sOpenMode, thisApp.iNumOfRows, thisApp.iMemorySize)
@@ -79,15 +78,6 @@ local function OnLoadFile()
 
 	return thisApp.iMemorySize, sText
 end
-
--- ----------------------------------------------------------------------------
---
-local tUtf8Code =
-{
-	{ 0x00, 0x7f,  nil }, -- no change
-	{ 0x80, 0xbf, 0xc2 }, -- 80, bf },
-	{ 0xc0, 0xff, 0xc3 }, -- 80, bf }
-}
 
 -- ----------------------------------------------------------------------------
 --
@@ -108,7 +98,6 @@ local function OnEncode_UTF_8()
 	
 	local sSource = thisApp.sFileMemory
 	local ch
-	local tChars = { }
 	local iNumSet = 0
 	
 	-- process char by char
@@ -124,26 +113,16 @@ local function OnEncode_UTF_8()
 			-- first 127 bytes are std ascii
 			--
 			
-		elseif tUtf8Code[2][3] == ch or tUtf8Code[3][3] == ch then
+--		elseif tUtf8Code[2][3] == ch or tUtf8Code[3][3] == ch then
 			
-			-- UTF_8 point
-			--
-			fhTgt:write(_toChar(ch))
+--			-- UTF_8 point
+--			--
+--			fhTgt:write(_toChar(ch))
 
-			-- read next byte of pair
-			--
-			iIndex = iIndex + 1
-			ch = sSource:sub(iIndex, iIndex):byte()			
-			
-		elseif tUtf8Code[2][1] <= ch and tUtf8Code[2][2] >= ch then	
-			
-			-- ASCII extended #1
-			--
-			fhTgt:write(_toChar(tUtf8Code[2][3]))
-			
-			ch = 0x80 + (ch - tUtf8Code[2][1])
-
-			iNumSet = iNumSet + 1			
+--			-- read next byte of pair
+--			--
+--			iIndex = iIndex + 1
+--			ch = sSource:sub(iIndex, iIndex):byte()		
 			
 		elseif tUtf8Code[3][1] <= ch and tUtf8Code[3][2] >= ch then	
 			
@@ -152,6 +131,16 @@ local function OnEncode_UTF_8()
 			fhTgt:write(_toChar(tUtf8Code[3][3]))
 			
 			ch = 0x80 + (ch - tUtf8Code[3][1])
+
+			iNumSet = iNumSet + 1				
+			
+		elseif tUtf8Code[2][1] <= ch and tUtf8Code[2][2] >= ch then	
+			
+			-- ASCII extended #1
+			--
+			fhTgt:write(_toChar(tUtf8Code[2][3]))
+			
+			ch = 0x80 + (ch - tUtf8Code[2][1])
 
 			iNumSet = iNumSet + 1			
 
@@ -166,23 +155,144 @@ local function OnEncode_UTF_8()
 	end
 
 	fhTgt:close()
-		
-	local sText = _format("File encoded UTF_8 with %d conversions", iNumSet)
-	trace.line(sText)	
 	
 	trace.lnTimeEnd("Encoding conversion")
 	
-	collectgarbage()
+	collectgarbage()	
+
+	local sText = _format("File encoded UTF_8 with %d conversions", iNumSet)
+	trace.line(sText)	
 	
 	return iNumSet, sText
 end
 
 -- ----------------------------------------------------------------------------
 --
+local function OnCheckEncodingNew()
+	
+--	local iLimit = thisApp.iMemorySize
+	if 0 >= thisApp.iMemorySize then return 0, "Nothing in memory" end
+	
+	trace.lnTimeStart("Check file encoding ...")
+	
+	local sSource	= thisApp.sFileMemory
+--	local dwCurHigh
+--	local dwCurLow
+	local sCurLine
+	local iLimit
+	local iStart 	= 1
+	local iEnd	 	= sSource:find("\n", iStart, true)
+	local iLineNo	= 0
+	local iCheckLow = 1
+	local iNumUTF_8 = 0
+	local iNumASCII = 0
+	local iNumError = 0
+	
+	-- check for unprintable characters
+	-- 1 or 2 are allowed (cr or cr/lf)
+	--
+	if thisApp.tConfig.ReadMode == "rb" then iCheckLow = 2 end
+		
+	local tCounters = {0, 0, 0, 0}		-- chars found on each segment
+	local tCtrsTot  = {0, 0, 0, 0}		-- sum up line by line
+	local tFileFmt  = {0, 0, 0}			-- sum up of ASCII, UTF_8, Err line by line
+
+	-- process line by line
+	--
+	while iEnd do
+		
+		sCurLine = sSource:sub(iStart, iEnd)
+		iLimit	= rawlen(sCurLine)
+		
+		
+		local i = 1
+		while i <= iLimit do
+		
+			local tConvert = nil
+			
+			if i <= (iLimit - 1) then
+				
+				tConvert = get2BytesRow(sCurLine:sub(i, i + 1))
+				
+				if tConvert then
+					
+					iNumUTF_8 = iNumUTF_8 + 1
+					
+					-- skip next
+					--
+					i = i + 1						
+				end
+			end
+			
+			if not tConvert then
+				
+				-- get the char code
+				--
+				tConvert = get1ByteRow(sCurLine:sub(i, i))
+				
+				if not tConvert then
+					iNumError = iNumError + 1
+				else
+					iNumASCII = iNumASCII + 1
+					
+					-- here we can append a char for writing the output file
+					--
+					-- check is ok
+					
+				end
+			end
+
+			-- process next
+			--
+			i = i + 1
+		
+		end
+		
+		-- save totals
+		--
+		for j=1, #tCtrsTot do tCtrsTot[j] = tCtrsTot[j] + tCounters[j] end
+		
+		tFileFmt[1] = tFileFmt[1] + iNumASCII
+		tFileFmt[2] = tFileFmt[2] + iNumUTF_8
+		tFileFmt[3] = tFileFmt[3] + iNumError
+
+		-- next line
+		--
+		tCounters 	= {0, 0, 0, 0}
+		iNumASCII 	= 0
+		iNumUTF_8 	= 0
+		iNumError	= 0
+		iLineNo		= iLineNo + 1
+		
+		iStart = iEnd + 1
+		iEnd	 = sSource:find("\n", iStart, true)		
+
+	end
+
+	trace.lnTimeEnd("Encoding check")
+	
+	-- number of characters below 0x20 (space) should be equal to the newline counter
+	--
+	local sText = _format("Totals: [0xc0: %d] [0x80: %d] [0x20: %d] [0x00: %d/%d]", 
+								 tCtrsTot[1], tCtrsTot[2], tCtrsTot[3], tCtrsTot[4], (iCheckLow * iLineNo))
+	trace.line(sText)	
+	
+	sText = _format("Summary: [iNumASCII: %d] [iNumUTF_8: %d] [iNumError: %d]", 
+						 tFileFmt[1], tFileFmt[2], tFileFmt[3])
+	
+	trace.line(sText)
+	
+	collectgarbage()
+	
+	return iLineNo, sText	
+end
+
+-- ----------------------------------------------------------------------------
+--
 local function OnCheckEncoding()
 	
-	local iLimit = thisApp.iMemorySize
-	if 0 >= iLimit then return 0, "Nothing in memory" end
+--	local iLimit = thisApp.iMemorySize
+	if 0 >= thisApp.iMemorySize then return 0, "Nothing in memory" end
 	
 	trace.lnTimeStart("Check file encoding ...")
 	
@@ -213,7 +323,7 @@ local function OnCheckEncoding()
 	while iEnd do
 
 		sCurLine = sSource:sub(iStart, iEnd)
-		iLimit	= sCurLine:len()
+		iLimit	= rawlen(sCurLine)
 		
 		bSkipNext = false
 		
@@ -302,6 +412,7 @@ local function OnCheckEncoding()
 	trace.line(sAssert)	
 	
 	-- number of characters below 0x20 (space) should be equal to the newline counter
+	-- otherwise there mught be tabulations or the file was read binary
 	--
 	local sText = _format("Totals: [0xc0: %d] [0x80: %d] [0x20: %d] [0x00: %d/%d]", 
 								 tCtrsTot[1], tCtrsTot[2], tCtrsTot[3], tCtrsTot[4], (iCheckLow * iLineNo))
@@ -322,55 +433,23 @@ local function OnCreateTest()
 	local sTargetFile = thisApp.tConfig.OutFile
 	local sOpenMode = thisApp.tConfig.WriteMode
 	
-	local fhTgt = io.open(sTargetFile, sOpenMode)
-	if not fhTgt then	
-		
-		trace.line("Failed to create [" .. sTargetFile .. "]")
-		return 0	
-	end
-	
-	local iLines	= 255
-	local iFormat	= 1
-
 	-- check which encoding to use
 	--
-	if thisApp.tConfig.TestEncode == "ASCII" then
-		iFormat = 1
-	elseif thisApp.tConfig.TestEncode == "UTF_8" then
-		iFormat = 2
-	end
-	
-	-- write the sequence
-	--
-	for i=1, iLines do
-		
-		if 1 == iFormat then
-			fhTgt:write(_toChar(i))
-			
-		elseif 2 == iFormat then
-			
-			if tUtf8Code[1][1] <= i and i <= tUtf8Code[1][2] then
-				fhTgt:write(_toChar(i))	
-				
-			elseif tUtf8Code[2][1] <= i and i <= tUtf8Code[2][2] then
-				fhTgt:write(_toChar(tUtf8Code[2][3]))
-				fhTgt:write(_toChar(0x80 + i - tUtf8Code[2][1]))
-				
-			elseif tUtf8Code[3][1] <= i and i <= tUtf8Code[3][2] then
-				fhTgt:write(_toChar(tUtf8Code[3][3]))
-				fhTgt:write(_toChar(0x80 + i - tUtf8Code[3][1]))				
-			else
-			end
-		else
-		end
-		
-		fhTgt:write(_toChar(0x0a))
-	end
-	fhTgt:close()
-	
-	trace.line("Test file in [" .. sTargetFile .. "]")
+	local iFormat	= 1
 
-	return iLines
+	if thisApp.tConfig.TestEncode == "UTF_8" then iFormat = 2 end
+	
+	trace.lnTimeStart("Creating samples file ...")
+	
+	-- call the function to create the samples
+	--
+	local bRet = samples.Create(sTargetFile, sOpenMode, iFormat)
+	
+	trace.lnTimeEnd("Create file end")
+	
+	collectgarbage()
+	
+	return bRet
 end
 
 -- ----------------------------------------------------------------------------
@@ -381,7 +460,7 @@ local function SetUpApplication()
 	trace.line(thisApp.sAppName .. " (Ver. " .. thisApp.sAppVersion .. ")")
 	trace.line("Released " .. thisApp.sAppRelDate)
 
-	rnd.initialize()
+--	rnd.initialize()
 	
 	assert(os.setlocale('ita', 'all'))
 	trace.line("Current locale is [" .. os.setlocale() .. "]")
@@ -400,7 +479,7 @@ local function QuitApplication()
 	
 	-- call the close 
 	--
-	CloseMainWindow()
+	window.CloseMainWindow()
 	
 	trace.line(thisApp.sAppName .. " terminated")
 end
@@ -411,10 +490,10 @@ local function main()
   
 	-- redirect logging
 	--
-	io.output("encodelog.txt")
+	io.output(thisApp.sLogFilename)
 
 	if SetUpApplication() then 
-		ShowMainWindow()
+		window.ShowMainWindow()
 	end
 
 	-- we'll get here only when the main window loop closes
@@ -428,21 +507,22 @@ end
 --
 thisApp =
 {
-	sAppVersion			= "0.0.3",					-- application's version
-	sAppRelDate			= "10-nov-18",				-- date of release update
-	sAppName				= "Encode",					-- name for the application
+	sAppVersion		= "0.0.3",				-- application's version
+	sAppRelDate		= "10-nov-18",			-- date of release update
+	sAppName		= "Encode",				-- name for the application
 	
-	sConfigIni			= "SetupInf.lua",			-- filename for the config
-	tConfig				= { },						-- configuration for the app.
+	sLogFilename	= "Encode.log",			-- logging filename
+	sConfigIni		= "SetupInf.lua",		-- filename for the config
+	tConfig			= { },					-- configuration for the app.
 	
-	sFileMemory			= "",							-- store file here
-	iMemorySize			= 0,							-- lenght of memory for the file
-	iNumOfRows			= 0,							-- number of new lines chars
+	sFileMemory		= "",					-- store file here
+	iMemorySize		= 0,					-- lenght of memory for the file
+	iNumOfRows		= 0,					-- number of new lines chars
 	
-	LoadFile				= OnLoadFile,				-- load the config file in memory
-	CheckEncoding		= OnCheckEncoding,		-- check chars in current file
-	Encode_UTF_8		= OnEncode_UTF_8,			-- save the current file UTF_8
-	CreateTest			= OnCreateTest,			-- create a 256 lines binary file
+	LoadFile		= OnLoadFile,			-- load the config file in memory
+	CheckEncoding	= OnCheckEncodingNew,	-- check chars in current file
+	Encode_UTF_8	= OnEncode_UTF_8,		-- save the current file UTF_8
+	CreateTest		= OnCreateTest,			-- create a 256 lines binary file
 }
 
 -- ----------------------------------------------------------------------------
