8f909d5dbbd488a29dac4ff32a5b2163ef865b34
decuant <decuant@gmail.com>
2018-11-20 11:13:20 +0100

    Update setupinf.lua

--- setupinf.lua
+++ setupinf.lua
@@ -1,30 +1,36 @@
--------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------
+--
+-- Setupinf - setup for the application
+--
+-- ----------------------------------------------------------------------------
 
 local tSetupInf =
 {
 	-- importing the file
 	--
-	["InFile"]		= "testfiles\\__Test_4.txt",
+	["InFile"]		= "testfiles\\__Test_2.txt",
 	["ReadMode"]	= "r",
 
 	-- saving the file
 	--
 	["OutFile"]		= "testfiles\\__Test_0.txt",
 	["WriteMode"]	= "w",
-	
-	-- test file
+
+	-- samples file
 	--
-	["TestEncode"]	= "UTF_8",								-- ASCII/UTF_8
+	["TestEncode"]	= "UTF_8",							-- ASCII/UTF_8
 
 	-- display
 	--
-	["ByteFont"]	= {12, "Source Code Pro"},			-- left display (codes)
-	["TextFont"]	= {11, "Lucida Sans Unicode"},	-- right display (text)
-	["Columns"]		= 16,										-- format number of columns
-	["Format"]		= "Hexadecimal",						-- Oct/Dec/Hex
+	["ByteFont"]	= {13, "Liberation Mono"},			-- left display (codes)
+	["TextFont"]	= {17, "Source Code Pro"}, -- "Carlito"},					-- right display (text)
+	["Columns"]		= 16,								-- format number of columns
+	["Format"]		= "Hexadecimal",					-- Oct/Dec/Hex
 }
 
+-- ----------------------------------------------------------------------------
+--
 return tSetupInf
 
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------
