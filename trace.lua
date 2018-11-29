dcf2c3c6576ae7b3f5eb9e50c77151bb624cdd68
decuant <decuant@gmail.com>
2018-11-29 11:06:19 +0100

    Update trace.lua
    removed unused functions

--- trace.lua
+++ trace.lua
@@ -29,7 +29,7 @@ end
 
 -- ----------------------------------------------------------------------------
 --
-function Trace.append(inMessage)
+function Trace.cat(inMessage)
 	if not inMessage then return end
 	
 	_write(inMessage)
@@ -77,78 +77,6 @@ function Trace.lnTimeEnd(inMessage)
 	Trace.line(_format("%s (%.04f s.)", inMessage, (mTickTimed - mTickStart)))
 	
 	mTickStart = mTickTimed
-end
-
---------------------------------------------------------------------------------
--- dump a buffer
---
-function Trace.dump(_title, buf)
-  
-	local blockText = "---- [" .. _title .. "] ----\n"  
-	_write(blockText)
-
-	for iIndex=1, #buf, 16 do
-		
-		local chunk = buf:sub(iIndex, iIndex + 15)
-
-		_write(_format('%08X  ', iIndex - 1))
-
-		chunk:gsub('.', function (c) _write(_format('%02X ', string.byte(c))) end)
-		_write(string.rep(' ', 3 * (16 - #chunk)))
-		_write(' ', chunk:gsub('%c','.'), "\n") 
-	end
-
-	_write(blockText)
-	_flush()  
-	
-end
-
--- ----------------------------------------------------------------------------
---  print a table in memory
---
-function Trace.table(t)
-  
-  local print_r_cache = {}
-  
-  local function sub_print_r(t, indent)
-    
-    if (print_r_cache[tostring(t)]) then
-      
-      print(indent.."*"..tostring(t))
-      
-    else
-      
-      print_r_cache[tostring(t)] = true
-      
-      if (type(t)=="table") then
-        for pos,val in pairs(t) do
-          if (type(val)=="table") then
-            print(indent.."["..pos.."] => "..tostring(t).." {")
-            sub_print_r(val,indent..string.rep(" ",string.len(pos)+4))
-            print(indent..string.rep(" ",string.len(pos)+2).."}")
-          elseif (type(val)=="string") then
-            print(indent.."["..pos..'] => "'..val..'"')
-          else
-            print(indent.."["..pos.."] => "..tostring(val))
-          end
-        end
-      else
-        print(indent..tostring(t))
-      end
-    end
-  end
-  
-  if (type(t)=="table") then
-    print(tostring(t).." {")
-    sub_print_r(t,"  ")
-    print("}")
-  else
-    sub_print_r(t,"  ")
-  end
-  
-  print()
-  _flush()
-  
 end
 
 -- ----------------------------------------------------------------------------
